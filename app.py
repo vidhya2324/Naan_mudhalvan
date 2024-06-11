be9bb1fae72810437007e335b582ebf5f4d4004a
Vidhya S <164010396+vidhya2324@users.noreply.github.com>
2024-06-11 00:02:35 -0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,61 @@
+app.py:
+
+from flask import Flask, render_template, redirect, url_for, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    text = db.Column(db.String(500), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            return redirect(url_for('chat'))
+    return render_template('login.html')
+
+@app.route('/chat')
+@login_required
+def chat():
+    messages = Message.query.all()
+    return render_template('chat.html', username=current_user.username, messages=messages)
+
+@app.route('/send', methods=['POST'])
+@login_required
+def send():
+    message = Message(user_id=current_user.id, text=request.form['message'])
+    db.session.add(message)
+    db.session.commit()
+    return redirect(url_for('chat'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
